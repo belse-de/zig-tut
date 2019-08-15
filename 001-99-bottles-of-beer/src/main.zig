3577db1b443eabb40243198476144d85cc099822
belse-de <belse.de@gmail.com>
2019-08-16 00:35:03 +0200

    99 bottles clean up

--- 001-99-bottles-of-beer/src/main.zig
+++ 001-99-bottles-of-beer/src/main.zig
@@ -2,53 +2,36 @@ const std = @import("std");
 
 pub fn main() anyerror!void {
     // If this program is run without stdout attached, exit with an error.
+    //This does not work: var out_stream = try std.io.getStdOut().outStream();
     const out_file = try std.io.getStdOut();
     var out_stream = out_file.outStream();
     var out = &out_stream.stream;
     
-    var bottles: i8 = 99;
-    while(bottles >= 0){
+    var bottles: u8 = 99;
+    while(bottles > 2){
         // If this program encounters pipe failure when printing to stdout, exit
         // with an error.
-        switch (bottles) {
-            1 => {
-                try out.print("{} bottle of beer on the wall, ", bottles);
-                try out.print("{} bottle of beer.\n", bottles); 
-            },
-            0 => {
-                try out.print("{} bottles of beer on the wall, ", "No more");
-                try out.print("{} bottles of beer.\n", "no more");
-            },
-            else => {
-                try out.print("{} bottles of beer on the wall, ", bottles);
-                try out.print("{} bottles of beer.\n", bottles);
-            },
-        }
-        
-        switch (bottles) {
-            0 => {
-                try out.print("Go to the store and buy some more, ");
-            },
-            else => {
-                try out.print("Take one down and pass it around, ");
-            },
-        }
-        
-        bottles -= 1;
-        
-        switch (bottles) {
-            1 => {
-                try out.print("{} bottle of beer on the wall.\n\n", bottles);    
-            },
-            0 => {
-                try out.print("{} bottles of beer on the wall.\n\n", "no more");    
-            },
-            -1 => {
-                try out.print("{} bottles of beer on the wall.\n\n", i8(99));    
-            },
-            else => {
-                try out.print("{} bottles of beer on the wall.\n\n", bottles);  
-            },
-        }
+        try out.print("{} bottles of beer on the wall, ", bottles);
+        try out.print("{} bottles of beer.\n", bottles);
+        try out.print("Take one down and pass it around, ");
+        bottles -= 1;       
+        try out.print("{} bottles of beer on the wall.\n\n", bottles);  
     }
+    
+    try out.print("{} bottles of beer on the wall, ", bottles);
+    try out.print("{} bottles of beer.\n", bottles);
+    try out.print("Take one down and pass it around, ");
+    bottles -= 1;
+    try out.print("{} bottle of beer on the wall.\n\n", bottles); 
+    
+    try out.print("{} bottle of beer on the wall, ", bottles);
+    try out.print("{} bottle of beer.\n", bottles); 
+    try out.print("Take one down and pass it around, ");
+    bottles -= 1;
+    try out.print("{} bottles of beer on the wall.\n\n", "no more");
+    
+    try out.print("{} bottles of beer on the wall, ", "No more");
+    try out.print("{} bottles of beer.\n", "no more");
+    try out.print("Go to the store and buy some more, ");
+    try out.print("{} bottles of beer on the wall.\n\n", u8(99)); 
 }
