e21570a7062bdf1f6c810366e64d8217f1b564b5
belse-de <belse.de@gmail.com>
2019-08-16 13:46:51 +0200

    made 99 bottles shorter

--- 002-99-bottles-of-beer/src/main.zig
+++ 002-99-bottles-of-beer/src/main.zig
@@ -8,27 +8,25 @@ pub fn main() anyerror!void {
     var out = &out_stream.stream;
     
     var bottles: u8 = 99;
-    while(bottles > 2){
-        // If this program encounters pipe failure when printing to stdout, exit
-        // with an error.
-        try out.print("{} bottles of beer on the wall, ", bottles);
-        try out.print("{} bottles of beer.\n", bottles);
+    var text_buffer: [16]u8 = "99 bottles" ++ []u8{0} ** 6;
+    var text = text_buffer[0..10];
+    while(bottles > 0){
+        // If this program encounters pipe failure when printing to stdout, 
+        // exit with an error.
+        
+        try out.print("{} of beer on the wall, ", text);
+        try out.print("{} of beer.\n", text);
         try out.print("Take one down and pass it around, ");
-        bottles -= 1;       
-        try out.print("{} bottles of beer on the wall.\n\n", bottles);  
+        
+        bottles -= 1;
+        text = switch(bottles){
+            0 =>  try std.fmt.bufPrint(text_buffer[0..], "{} bottles", "no more"),
+            1 => try std.fmt.bufPrint(text_buffer[0..], "{} bottle", bottles),
+            else => try std.fmt.bufPrint(text_buffer[0..], "{} bottles", bottles),
+        };
+        
+        try out.print("{} of beer on the wall.\n\n", text);
     }
-    
-    try out.print("{} bottles of beer on the wall, ", bottles);
-    try out.print("{} bottles of beer.\n", bottles);
-    try out.print("Take one down and pass it around, ");
-    bottles -= 1;
-    try out.print("{} bottle of beer on the wall.\n\n", bottles); 
-    
-    try out.print("{} bottle of beer on the wall, ", bottles);
-    try out.print("{} bottle of beer.\n", bottles); 
-    try out.print("Take one down and pass it around, ");
-    bottles -= 1;
-    try out.print("{} bottles of beer on the wall.\n\n", "no more");
     
     try out.print("{} bottles of beer on the wall, ", "No more");
     try out.print("{} bottles of beer.\n", "no more");
