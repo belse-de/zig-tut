25e975c356b632fafa2627b6e6b08a2b3b7c1d30
belse-de <belse.de@gmail.com>
2019-08-19 23:32:08 +0200

    brainfuck - added loop functions

--- 004-brainfuck/src/main.zig
+++ 004-brainfuck/src/main.zig
@@ -5,6 +5,60 @@ const io = std.io;
 const warn = std.debug.warn;
 const assert = std.debug.assert;
 
+fn jumpForward(prog: []const u8, prog_ptr: usize) usize {
+    var bracket_stack: usize = 1;
+    var ptr: usize = prog_ptr;
+    
+    while(bracket_stack > 0) {
+        ptr += 1;
+        // if (ptr >= prog.len) // expected matching
+        switch (prog[ptr]) {
+            '[' => bracket_stack += 1,
+            ']' => bracket_stack -= 1,
+            else => {},
+        }
+    }
+    
+    return ptr;
+}
+
+test "jumpForward" {
+    assert(jumpForward("[]", 0) == 1);
+    assert(jumpForward("[[]]", 0) == 3);
+    assert(jumpForward("[[]]", 1) == 2);
+    assert(jumpForward("[[][]]", 0) == 5);
+    assert(jumpForward("[[][]]", 1) == 2);
+    assert(jumpForward("[[][]]", 3) == 4);
+}
+
+
+fn jumpBackward(prog: []const u8, prog_ptr: usize) usize {
+    var bracket_stack: usize = 1;
+    var ptr: usize = prog_ptr;
+    
+    while(bracket_stack > 0) {
+        ptr -= 1;
+        // if (ptr >= prog.len) // expected matching
+        switch (prog[ptr]) {
+            '[' => bracket_stack -= 1,
+            ']' => bracket_stack += 1,
+            else => {},
+        }
+    }
+    
+    return ptr;
+}
+
+test "jumpBackward" {
+    assert(jumpBackward("[]", 1) == 0);
+    assert(jumpBackward("[[]]", 3) == 0);
+    assert(jumpBackward("[[]]", 2) == 1);
+    assert(jumpBackward("[[][]]", 5) == 0);
+    assert(jumpBackward("[[][]]", 2) == 1);
+    assert(jumpBackward("[[][]]", 4) == 3);
+}
+
+
 fn bf(prog: []const u8, mem: []u8) !void {
     const in_file = try io.getStdIn();
     const out_file = try io.getStdOut();
@@ -24,8 +78,12 @@ fn bf(prog: []const u8, mem: []u8) !void {
             ',' => {
                 _ = try in_file.read(mem[mem_ptr..mem_ptr]);
             },
-            //'[' => {},
-            //']' => {},
+            '[' => if (mem[mem_ptr] == 0) {
+              prog_ptr = jumpForward(prog, prog_ptr);
+            },
+            ']' => if (mem[mem_ptr] != 0) {
+              prog_ptr = jumpBackward(prog, prog_ptr);
+            },
             else => {},
         }
         prog_ptr += 1;
